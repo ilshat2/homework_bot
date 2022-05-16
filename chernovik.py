362ccfc3b78314aef1e87795e0e7c8371b116c0c
Ильшат Кушманбетов <ilshat261093@yandex.ru>
2022-05-16 21:04:07 +0300

    Спринт 7 1.0
    * Черновой вариант на всякий случай

---
+++ chernovik.py
@@ -0,0 +1,123 @@
+def get_api_answer(current_timestamp):
+    """Делаем запрос к единственному эндпоинту API-сервиса.
+    """
+    #timestamp = current_timestamp or int(time.time())
+    timestamp = current_timestamp
+    params = {'from_date': timestamp}
+    answer_homework = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    print(answer_homework)
+    print(timestamp)
+    print('----------------')
+    if str(answer_homework) == '<Response [200]>':
+        answer_homework = answer_homework.json()
+        return answer_homework
+    answer_homework = False
+    print(answer_homework)
+    print('----------------')
+    return answer_homework
+
+def get_api_answer(current_timestamp):
+    """Делаем запрос к единственному эндпоинту API-сервиса.
+    """
+    #timestamp = current_timestamp or int(time.time())
+    timestamp = current_timestamp
+    params = {'from_date': timestamp}
+    answer_homework = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    print('ФУНКЦИЯ get_api_answer')
+    print(answer_homework)
+    print(timestamp)
+    print('----------------')
+    print()
+    if answer_homework.status_code == HTTPStatus.OK:
+        answer_homework = answer_homework.json()
+        return answer_homework
+    
+    elif answer_homework.status_code == HTTPStatus.NOT_FOUND:
+        print('ERORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR')
+        return TypeError
+    elif answer_homework.status_code != HTTPStatus.OK:
+        return TypeError
+
+
+
+
+def check_tokens():
+    """Проверяем доступность переменных окружения,
+    которые необходимы для работы программы.
+    """
+    if 'TOKEN_P' and 'TOKEN_T' and 'CHAT_ID' in os.environ:
+    if os.environ['TOKEN_P'] == PRACTICUM_TOKEN and os.environ['TOKEN_T'] == TELEGRAM_TOKEN and os.environ['CHAT_ID'] == TELEGRAM_CHAT_ID:
+        return True
+    return False
+
+
+
+
+def check_response(response):
+    """Проверяем ответ API на корректность.
+    """
+    try:
+        response = response['homeworks']
+        response = response[0]
+    except KeyError:
+        response = False
+    except IndexError:
+        response = False
+    print('ФУНКЦИЯ check_response')
+    print(response)
+    print(type(response))
+    print('----------------')
+    print()
+    if type(response) == dict:
+        return response
+    return False
+
+
+
+
+def main():
+    """Основная логика работы бота.
+    """
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    current_timestamp = 0 
+    #current_timestamp = int(time.time())
+
+    check_tokens()
+    print('ФУНКЦИЯ main')
+    print(check_tokens())
+    print('----------------')
+    print()
+    result_get_api_answer = get_api_answer(current_timestamp)
+    result_check_response = check_response(result_get_api_answer)
+    result_parse_status = parse_status(result_check_response)
+    send_message(bot, result_parse_status)
+
+
+    updater = Updater(token=TELEGRAM_TOKEN)
+    updater.start_polling(poll_interval=RETRY_TIME)
+    updater.idle()
+    ...
+
+
+
+    ...
+
+    while True:
+        try:
+            response = ...
+
+            ...
+
+            current_timestamp = ...
+            time.sleep(RETRY_TIME)
+
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            ...
+            time.sleep(RETRY_TIME)
+        else:
+            ...
+
+
+if __name__ == '__main__':
+    main()
--- homework.py
+++ homework.py
@@ -1,11 +1,25 @@
-...
+import os
+import time
+import requests
+import logging
+
+import telegram
+from dotenv import load_dotenv
+from telegram.ext import Updater
+from http import HTTPStatus
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('TOKEN_P')
+TELEGRAM_TOKEN = os.getenv('TOKEN_T')
+TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
+print('ТОКЕНЫЫЫЫЫЫЫЫЫЫЫЫ')
+print(PRACTICUM_TOKEN)
+print(TELEGRAM_TOKEN)
+print(TELEGRAM_CHAT_ID)
+print('----------------')
+print()
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,65 +32,149 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='program.log',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
+
 
 def send_message(bot, message):
-    ...
+    """Отправляем сообщение в Telegram чат."""
+    bot.send_message(TELEGRAM_CHAT_ID, message)
+    logging.info('Функция send_message. Сообщение отправлено.')
 
 
 def get_api_answer(current_timestamp):
+    """Делаем запрос к единственному эндпоинту API-сервиса."""
     timestamp = current_timestamp or int(time.time())
+    #  timestamp = current_timestamp
     params = {'from_date': timestamp}
-
-    ...
+    try:
+        logging.info('Функция get_api_answer, отправляем api-запрос.')
+        answer_homework = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params=params
+        )
+    except ValueError as error:
+        logging.error(f'Функция get_api_answer, ошибка {error}.')
+        raise error
+    print('ФУНКЦИЯ get_api_answer')
+    print(answer_homework)
+    print(timestamp)
+    print('----------------')
+    print()
+    get_api_answer_error = (
+        f'Функция get_api_answer, ошибка {answer_homework.status_code}.'
+    )
+    if answer_homework.status_code == HTTPStatus.OK:
+        return answer_homework.json()
+    else:
+        raise TypeError(get_api_answer_error)
 
 
 def check_response(response):
-
-    ...
+    """Проверяем ответ API на корректность."""
+    logging.info(
+        'Функция check_response, проверяем ответ API на корректность.'
+    )
+    print('ФУНКЦИЯ check_response')
+    try:
+        response = response['homeworks']
+        print('1', response)
+    except KeyError:
+        logging.error('Функция check_response, неверный ключ.')
+        response = None
+    try:
+        response = response[0]
+        print('2', response)
+    except IndexError:
+        logging.error('Функция check_response, неверный индекс.')
+        response = None
+    print('3', response)
+    print(type(response))
+    print('----------------')
+    print()
+    if type(response) == dict:
+        return response
+    logging.error('Функция check_response, неверный тип возвращаемых данных.')
+    return None
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    """Из информации о конкретной домашней работе извдекаем статус."""
+    logging.info(
+        'Функция parse_status, извлекли информацию о конкретной работе.'
+    )
 
-    ...
-
-    verdict = ...
-
-    ...
-
+    #  homework = homework[0]
+    try:
+        homework_name = homework['homework_name']
+        homework_status = homework['status']
+        verdict = HOMEWORK_STATUSES[homework_status]
+    except KeyError:
+        homework_name = None
+    except TypeError:
+        homework_name = None
+    if 'homework_name' not in homework:
+        logging.error(
+            'Функция parse_status, homework_name отсутствует в homework'
+        )
+        raise KeyError(
+            'Функция parse_status, homework_name отсутствует в homework'
+        )
+    if homework_status not in HOMEWORK_STATUSES:
+        raise Exception
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    """Проверяем доступность переменных окружения,
+    которые необходимы для работы программы.
+    """
+    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID is not None:
+        logging.info('Функция check_tokens, переменные окружения доступны.')
+        return True
+    logging.critical('Функция check_tokens, переменные окружения не доступны.')
+    return False
 
 
 def main():
-    """Основная логика работы бота."""
-
-    ...
-
+    """Основная логика работы бота.
+    """
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
-
-    ...
-
+    #  current_timestamp = 0
+    current_timestamp = 1650130498
+    check_tokens()
+    print('ФУНКЦИЯ main')
+    print(check_tokens())
+    print('----------------')
+    print()
+    error_message = ''
     while True:
         try:
-            response = ...
+            response = get_api_answer(current_timestamp)
+            result_check_response = check_response(response)
+            message = parse_status(result_check_response)
 
-            ...
-
-            current_timestamp = ...
+            current_timestamp = response.get('current_date', current_timestamp)
+            if result_check_response:
+                send_message(bot, parse_status(result_check_response))
             time.sleep(RETRY_TIME)
-
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
+            if message not in error_message:
+                error_message = message
+                logging.critical(message)
+                send_message(bot, message)
+            #  logging.error(message)
             time.sleep(RETRY_TIME)
         else:
-            ...
+            send_message(bot, message)
+            updater = Updater(token=TELEGRAM_TOKEN)
+            updater.start_polling(poll_interval=RETRY_TIME)
+            updater.idle()
 
 
 if __name__ == '__main__':
